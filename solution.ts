526e9e34dfe022b26fed991e5f42f30ea541e477
Mehedi <mh0626254@gmail.com>
2026-01-31 02:22:43 +0600

    Problem1,2,3 are done

--- solution.ts
+++ solution.ts
@@ -1,1 +1,45 @@
 //Problem 1
+// const formatValue = (value : number | string | boolean) =>{
+//     if(typeof value=== 'string'){
+//         return value.toString().toLocaleUpperCase();
+//     }
+//     else if(typeof value ==='number'){
+//         return value*10;
+//     }
+//     else{
+//         return !value;
+//     }
+// }
+
+
+//Problem 2
+// const getLength = (value : string | number[]) =>{
+//     if(Array.isArray(value)){
+//         return value.length;
+//     }
+//     else{
+//         return value.length
+//     }
+// }
+
+//Problem 3
+
+// class Person {
+//     name : string;
+//     age : number;
+
+//     constructor(name: string, age: number){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     getDetails(){
+//       return `Name: ${this.name}, Age: ${this.age}`
+//     }
+// }
+
+
+//Problem 4
+
+
+
